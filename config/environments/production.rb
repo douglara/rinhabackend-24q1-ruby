eee6aa7806956a4d6314544459256b291e24ec4c
Douglas Lara <douglasdelaras@gmail.com>
2024-02-11 21:12:13 -0300

    Basic environment setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,17 +45,29 @@ Rails.application.configure do
   config.force_ssl = false
 
   # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  if ENV['RAILS_LOG_TO_STDOUT'] == 'true'
+    config.logger = ActiveSupport::Logger.new(STDOUT)
+      .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+      .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
-  # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+    # Prepend all log lines with the following tags.
+    config.log_tags = [ :request_id ]
 
-  # "info" includes generic and useful information about system operation, but avoids logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII). If you
-  # want to log everything, set the level to "debug".
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+    # "info" includes generic and useful information about system operation, but avoids logging too much
+    # information to avoid inadvertent exposure of personally identifiable information (PII). If you
+    # want to log everything, set the level to "debug".
+    config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  else
+    config.middleware.delete Rails::Rack::Logger
+  end
+
+  config.middleware.delete Rack::Sendfile
+  config.middleware.delete ActionDispatch::Static
+  config.middleware.delete ActionDispatch::RequestId
+  config.middleware.delete ActionDispatch::RemoteIp
+  config.middleware.delete ActionDispatch::ShowExceptions
+  config.middleware.delete ActionDispatch::DebugExceptions
+  config.middleware.delete Middleware::ConsiderAllRequestJsonMiddleware
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
